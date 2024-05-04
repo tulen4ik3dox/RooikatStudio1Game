c8c8417e77eaf16f56ab7549cec7e2f87efcbb3d
tulen4ik3dox <154080415+tulen4ik3dox@users.noreply.github.com>
2024-05-05 02:32:23 +0300

    05.05.24T

--- Assets/MainMenu/Scripts/GeneratorClew.cs
+++ Assets/MainMenu/Scripts/GeneratorClew.cs
@@ -5,19 +5,45 @@ public class GeneratorClew : MonoBehaviour
 
     public GameObject _RealyClew;
     public GameObject _CopyClew;
+    public GameObject _NewClew;
+    public float _force;
     public clew _script;
+    public Vector2 positionMouse0 = new Vector2(0, 0);
+    public int _plusOrMinus;
+    public bool _lauched;
+    public int _clewTo;
 
     void Update()
     {
-        transform.position = _RealyClew.transform.position;
-        if (_script._lauchCopy && _script._lauchIndex == 0)
+
+        Vector2 positionMouse = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        if (positionMouse0.x < positionMouse.x)
         {
-            Debug.Log("ÌßÓ");
-            GameObject _copy = Instantiate(_CopyClew);
-            _copy.transform.position = _RealyClew.transform.position;
-            _script._lauchIndex = 1;
+            _plusOrMinus = 1;
+        }
+        else if (positionMouse0.x > positionMouse.x)
+        {
+            _plusOrMinus = -1;
         }
 
-        
+        transform.position = _RealyClew.transform.position;
+
+
+        if (_script._lauchCopy && _script._lauchIndex == 0)
+        {
+            Debug.Log("AAAAAAAAAA");
+            FourceTo(_plusOrMinus);
+        }
+
+        positionMouse0 = positionMouse;
+
+    }
+    public void FourceTo(int _to)
+    {
+        GameObject _copy = Instantiate(_CopyClew, _NewClew.transform);
+        _copy.transform.position = transform.position;
+        _script._lauchIndex = 1;
+        _lauched = true;
+        _clewTo = _to;
     }
 }
